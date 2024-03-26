46480ba9a28167d9b2515f04b82a598288218093
Daniel Sloof <daniel@rubic.nl>
2024-03-26 10:31:05 +0100

    filter allowed sources

--- Model/Config.php
+++ Model/Config.php
@@ -9,6 +9,7 @@ class Config
 {
     private const CONFIG_PATH_ENDPOINT    = 'cspmon/settings/endpoint';
     private const CONFIG_PATH_SAMPLE_RATE = 'cspmon/settings/sample_rate';
+    private const CONFIG_PATH_DIRECTIVES  = 'cspmon/directives';
 
     private ?bool $shouldReport;
     private ScopeConfigInterface $scopeConfig;
@@ -38,8 +39,13 @@ class Config
         return $this->shouldReport;
     }
 
-    public function getCspEndpoint(): string
+    public function getEndpoint(): string
     {
         return $this->scopeConfig->getValue(self::CONFIG_PATH_ENDPOINT);
+    }
+
+    public function getDirectives(): array
+    {
+        return $this->scopeConfig->getValue(self::CONFIG_PATH_DIRECTIVES);
     }
 }
--- Observer/AddReportHeaders.php
+++ Observer/AddReportHeaders.php
@@ -15,22 +15,37 @@ class AddReportHeaders implements ObserverInterface
         $this->config = $config;
     }
 
+    private function getReportPolicy(): string
+    {
+        $directivePolicies = [];
+        foreach ($this->config->getDirectives() as $directive) {
+            $directivePolicies[] = sprintf(
+                "%s '%s';",
+                $directive['name'],
+                implode("' '", explode(',', $directive['allowed_sources'] ?? []))
+            );
+        }
+        return implode(' ', $directivePolicies);
+    }
+
     public function execute(Observer $observer)
     {
+        $this->getReportPolicy();
+
         $response = $observer->getEvent()->getData('response');
         if (!$this->config->shouldReport()) {
             return;
         }
 
         $response->setHeader(
-            'Reporting-Endpoints',
-            sprintf('csp-endpoint=\'%s\'', $this->config->getCspEndpoint()),
+            "Reporting-Endpoints",
+            sprintf('csp-endpoint="%s"', $this->config->getEndpoint()),
             true
         );
 
         $response->setHeader(
-            'Content-Security-Policy-Report-Only',
-            'script-src \'none\'; connect-src \'none\'; report-to csp-endpoint',
+            "Content-Security-Policy-Report-Only",
+            sprintf("%s report-to csp-endpoint", $this->getReportPolicy()),
             true
         );
     }
