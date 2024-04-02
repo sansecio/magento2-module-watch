b480bbb0ea73bd3f4f7309b5866b6b1c6c85c0dd
Daniel Sloof <daniel@rubic.nl>
2024-04-02 11:39:53 +0200

    move files to root

--- registration.php
+++ registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Sansec_Cspmon', __DIR__ . '/src');
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Sansec_Cspmon', __DIR__);
